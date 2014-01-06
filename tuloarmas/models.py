053098e62b0bec7ab54ba857ad91515ad11027c9
RicardoCisterna <ricardo.cisterna@usach.cl>
2014-01-06 12:15:12 -0300

    algo

--- tuloarmas/models.py
+++ tuloarmas/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 #class Producto(models.Model):
 #    id = models.AutoField('ID', primary_key=True)
